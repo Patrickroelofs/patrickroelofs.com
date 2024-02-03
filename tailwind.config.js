0428cf6c8e6e5833de5e398521806cb5915a1853
Patrick Roelofs <contact@patrickroelofs.com>
2024-02-03 17:19:39 +0100

    Add tailwindcss Fixes #2

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	plugins: [require('@tailwindcss/typography')],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	}
+};
