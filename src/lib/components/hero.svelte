0f172d98fa4df4d3750a075ba9a1615ade2c7de0
Patrick Roelofs <contact@patrickroelofs.com>
2024-02-07 14:05:28 +0100

    Fix leading-normal class in hero.svelte component

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,7 +1,7 @@
 <header class="mx-auto mt-12 flex h-auto max-w-7xl items-center lg:mt-36">
 	<div class="relative flex flex-col gap-8 lg:flex-row">
 		<p
-			class="z-10 max-w-6xl text-balance px-4 pt-0 text-center font-serif text-3xl leading-normal lg:px-0 lg:pt-32 lg:text-left lg:text-7xl"
+			class="z-10 max-w-6xl text-balance px-4 pt-0 text-center font-serif text-3xl leading-normal lg:px-0 lg:pt-32 lg:text-left lg:text-7xl lg:leading-normal"
 		>
 			A front-end developer with a passion for user experiences.
 		</p>
