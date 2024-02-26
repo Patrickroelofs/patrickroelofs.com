a4ceb870de1379daaac1d6e21125a4c886d2e7cd
Patrick Roelofs <contact@patrickroelofs.com>
2024-02-26 19:12:25 +0100

    Update hero component with image animation

--- src/lib/components/hero.component.svelte
+++ src/lib/components/hero.component.svelte
@@ -1,14 +1,35 @@
-<header class="mx-auto mt-12 flex h-auto max-w-7xl items-center lg:mt-36">
-	<div class="relative flex flex-col gap-8 lg:flex-row">
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { gsap } from 'gsap';
+
+	let image: HTMLImageElement;
+
+	onMount(() => {
+		window.addEventListener('mousemove', (e) => {
+			const x = (window.innerWidth / 2 - e.clientX) / 90;
+			const y = (window.innerHeight / 2 - e.clientY) / 90;
+
+			gsap.to(image, {
+				duration: 0.5,
+				x: x,
+				y: y,
+				ease: 'power1.out'
+			});
+		});
+	});
+</script>
+
+<header class="relative mx-auto mt-12 flex h-auto max-w-7xl items-center lg:mt-36">
+	<div class="flex flex-col gap-8 lg:flex-row">
 		<p
 			class="z-10 max-w-6xl text-balance px-4 pt-0 text-center font-serif text-3xl leading-normal lg:px-0 lg:pt-32 lg:text-left lg:text-7xl lg:leading-normal"
 		>
 			A front-end developer with a passion for user experiences.
 		</p>
-		<img
-			src="/images/patrick.jpg"
-			alt=""
-			class="right-0 top-0 aspect-square w-full self-end object-cover px-4 lg:absolute lg:-right-56 lg:w-1/2 lg:rounded-full lg:px-0"
-		/>
+		<div
+			class="right-0 mx-auto w-96 overflow-hidden bg-gradient-to-br from-hermes-200 to-hermes-300 lg:absolute lg:h-[512px] lg:w-[512px] lg:rounded-full"
+		>
+			<img bind:this={image} src="/images/patrick.png" alt="" />
+		</div>
 	</div>
 </header>
