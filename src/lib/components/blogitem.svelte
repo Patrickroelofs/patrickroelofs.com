4c9258eee99d620b1d24b6adec4b7326163ce625
Patrick Roelofs <contact@patrickroelofs.com>
2024-02-29 19:05:28 +0100

    Update blog and hero components

--- src/lib/components/blogitem.svelte
+++ src/lib/components/blogitem.svelte
@@ -36,19 +36,28 @@
 		on:focus={MouseOver}
 		on:focusout={MouseLeave}
 		href="#"
-		class="group relative flex items-center justify-between rounded-xl py-6 transition-all duration-300 ease-in-out hover:bg-hermes-200"
+		class="group relative flex items-center justify-between border-b-2 border-black py-6 transition-all duration-300 ease-in-out"
 	>
-		<p
-			class="text-4xl font-bold transition-all duration-300 ease-in-out group-hover:pl-4 md:text-6xl"
-		>
-			0{index + 1}
-		</p>
-		<p class="mr-4 w-1/3 text-xl md:text-2xl">{item.title}</p>
+		<div class="flex gap-16">
+			<p
+				class="text-4xl font-bold transition-all duration-300 ease-in-out group-hover:pl-4 md:text-8xl"
+			>
+				0{index + 1}
+			</p>
+			<div class="flex w-full max-w-xl flex-col gap-4">
+				<p class="mr-4 text-xl font-medium md:text-3xl">{item.title}</p>
+				<p
+					class="line-clamp-3 text-ellipsis leading-normal opacity-40 transition-all duration-300 ease-in-out group-hover:opacity-100"
+				>
+					{item.description}
+				</p>
+			</div>
+		</div>
 		<img
 			aria-hidden="true"
 			src="https://placekitten.com/256/144"
 			alt=""
-			class="pointer-events-none absolute right-1/4 hidden h-36 w-64 rounded-lg object-cover object-center opacity-0 shadow-2xl md:block"
+			class="pointer-events-none absolute right-64 hidden h-[236px] w-[420px] rounded-lg object-cover object-center opacity-0 shadow-2xl 2xl:block"
 		/>
 		<span class="flex w-1/3 justify-end transition-all duration-300 ease-in-out group-hover:pr-4">
 			<div class="w-8 md:w-16">
