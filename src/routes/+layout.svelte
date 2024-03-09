83849d13a91903c66d9535baa4c85b4cbfae1b38
Patrick Roelofs <contact@patrickroelofs.com>
2024-03-09 20:52:14 +0100

    Add head component and update layout with head component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,11 @@
 
 	import Backgroundelements from '../lib/components/backgroundelements.svelte';
 	import Navigation from '../lib/components/navigation.svelte';
+	import Head from '$lib/components/head.svelte';
 	import '../app.css';
 </script>
 
+<Head title="Patrick Roelofs" url="https://patrickroelofs.com" />
 <Navigation />
 <slot />
 <Backgroundelements />
