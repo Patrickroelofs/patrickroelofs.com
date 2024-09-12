acc4650daa32f061ac14cbcffe68816a04078f63
Patrick Roelofs <contact@patrickroelofs.com>
2024-09-12 23:30:28 +0200

    add astro
    Signed-off-by: Patrick Roelofs <contact@patrickroelofs.com>

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/static";
+
+export default defineConfig({
+  output: "static",
+  adapter: vercel(),
+});
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { type ReactNode } from 'react';
-import styles from './page.module.css';
-
-export default function Home(): ReactNode {
-  return <h1 className={styles.text}>Patrick Roelofs :)</h1>;
-}
