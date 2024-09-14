32794f2dd6596bf11cf13c378ce87a63f94672b0
Patrick Roelofs <contact@patrickroelofs.com>
2024-09-14 21:56:59 +0200

    remove storyblok attributes on production build
    Signed-off-by: Patrick Roelofs <contact@patrickroelofs.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { loadEnv } from "vite";
 import vercel from "@astrojs/vercel/static";
 import storyblok from "@storyblok/astro";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import { removeStoryblokAttributes } from "./src/utils/removeStoryblokAttributes";
 
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
@@ -20,7 +21,19 @@ export default defineConfig({
     }),
   ],
   vite: {
-    plugins: [basicSsl()],
+    plugins: [
+      basicSsl(),
+      {
+        name: "remove-data-blok-c",
+        enforce: "post",
+        transformIndexHtml(html) {
+          if (process.env.NODE_ENV === "production") {
+            return removeStoryblokAttributes(html);
+          }
+          return html;
+        },
+      },
+    ],
     server: {
       https: true,
     },
