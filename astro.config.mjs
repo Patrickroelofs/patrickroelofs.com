bc285131bbffc352801627f7c2ed4d26c65dc545
Patrick Roelofs <contact@patrickroelofs.com>
2024-09-14 21:26:21 +0200

    add storyblok
    Signed-off-by: Patrick Roelofs <contact@patrickroelofs.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
 import { defineConfig } from "astro/config";
 
+import { loadEnv } from "vite";
 import vercel from "@astrojs/vercel/static";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 export default defineConfig({
   output: "static",
   adapter: vercel(),
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_ACCESS_TOKEN,
+      components: {
+        page: "components/Page",
+        Text: "components/Text",
+      },
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
