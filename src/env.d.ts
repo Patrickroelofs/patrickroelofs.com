c11cc950b7496b221d127a978b2d2ed013d90e0f
Turbobot <turbobot@vercel.com>
2024-09-16 17:59:41 +0200

    feat(create-turbo): apply official-starter transform

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-
-interface ImportMetaEnv {
-  readonly STORYBLOK_ACCESS_TOKEN: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
