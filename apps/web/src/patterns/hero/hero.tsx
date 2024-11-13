ebe88fd98cf0e87494c67b382272c87c5e9fa4d7
Patrick Roelofs <contact@patrickroelofs.com>
2024-11-13 18:37:34 +0100

    feat: add base navigation links

--- apps/web/src/patterns/hero/hero.tsx
+++ apps/web/src/patterns/hero/hero.tsx
@@ -4,8 +4,8 @@ import heroImage from '@/images/hero.jpg';
 
 export function Hero(): ReactElement {
   return (
-    <header className="mx-auto pb-4 max-w-screen-2xl">
-      <div className="flex justify-center align-center">
+    <header className="mx-auto max-w-screen-2xl">
+      <div className="flex justify-center align-center px-xs">
         <Image src={heroImage} alt="" className="w-full mx-auto rounded-3xl" />
       </div>
     </header>
--- apps/web/src/patterns/navigation/navigation.tsx
+++ apps/web/src/patterns/navigation/navigation.tsx
@@ -1,15 +1,41 @@
-import { type ReactElement } from 'react';
-import Link from 'next/link';
+import React, { type ReactElement } from 'react';
+import Link, { type LinkProps } from 'next/link';
+import Image, { type StaticImageData } from 'next/image';
+import squiggly from '@/icons/squiggly.svg';
+
+function NavigationLink({
+  children,
+  ...props
+}: LinkProps & { children: React.ReactNode }): ReactElement {
+  return (
+    <li className="text-base">
+      <Link {...props}>{children}</Link>
+    </li>
+  );
+}
 
 export function Navigation(): ReactElement {
   return (
-    <nav className="flex justify-between sticky top-0 mt-4 mb-4 pt-4 pb-4">
-      <div className="max-w-screen-2xl mx-auto w-full px-4">
-        <div className="flex justify-between items-center">
-          <Link href="/" className="font-serif font-semibold text-4xl">
-            <h1>Patrick Roelofs</h1>
-          </Link>
-        </div>
+    <nav className="px-md flex max-w-screen-2xl mx-auto justify-between mt-xs mb-xs">
+      <div className="flex justify-between items-center w-full">
+        <Link
+          href="/"
+          className="font-serif font-semibold text-xl relative group"
+        >
+          <h1>
+            Patrick Roelofs
+            <span className="absolute -bottom-3xs w-full left-0 invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-150">
+              <Image src={squiggly as StaticImageData} alt="" />
+            </span>
+          </h1>
+        </Link>
+
+        <ul className="flex gap-sm">
+          <NavigationLink href="#">Projects</NavigationLink>
+          <NavigationLink href="#">Blog</NavigationLink>
+          <NavigationLink href="#">Photography</NavigationLink>
+          <NavigationLink href="#">Contact</NavigationLink>
+        </ul>
       </div>
     </nav>
   );
