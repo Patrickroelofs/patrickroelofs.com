f01c86b569e65a5d62ffa7063445cae11c854312
Patrick Roelofs <contact@patrickroelofs.com>
2024-11-16 22:17:08 +0100

    fix: build error

--- src/utils/revalidate.ts
+++ src/utils/revalidate.ts
@@ -18,12 +18,12 @@ export const revalidate = async (args: {
       );
     } else {
       payload.logger.error(
-        `Error revalidating page '${slug}' in collection '${collection}': ${res}`,
+        `Error revalidating page '${slug}' in collection '${collection}'`,
       );
     }
   } catch (err: unknown) {
     payload.logger.error(
-      `Error hitting revalidate route for page '${slug}' in collection '${collection}': ${err}`,
+      `Error hitting revalidate route for page '${slug}' in collection '${collection}'`,
     );
   }
 };
