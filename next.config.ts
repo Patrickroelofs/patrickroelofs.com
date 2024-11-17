d49509f7727bcb85c6c9c328cc9be1413cb27a7d
Patrick Roelofs <contact@patrickroelofs.com>
2024-11-17 12:03:08 +0100

    chore: add domains to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from 'next';
 import { withPayload } from '@payloadcms/next/withPayload';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'patrickroelofs.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'patrickroelofs.dev',
+      },
+    ],
+  },
 };
 
 export default withPayload(nextConfig);
