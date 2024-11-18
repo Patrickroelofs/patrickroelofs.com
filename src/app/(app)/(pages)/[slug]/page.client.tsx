34a7e04e51ba702d402fda34ea8ac8a8dfeb75cc
Patrick Roelofs <contact@patrickroelofs.com>
2024-11-18 19:26:51 +0100

    debug: deployment error

--- src/app/(app)/(pages)/[slug]/page.client.tsx
+++ src/app/(app)/(pages)/[slug]/page.client.tsx
@@ -9,10 +9,17 @@ function PageClient({ page }: { page: Page }): ReactElement {
   const { data } = useLivePreview<Page>({
     initialData: page,
     serverURL:
-      process.env.NEXT_PUBLIC_SERVER_URL ??
-      process.env.VERCEL_PROJECT_PRODUCTION_URL ??
-      '',
+      process.env.VERCEL_ENV === 'production'
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL ?? ''}`
+        : (process.env.NEXT_PUBLIC_SERVER_URL ?? ''),
   });
+
+  /* eslint-disable no-console -- testing purposes */
+  console.log(
+    process.env.VERCEL_ENV === 'production'
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL ?? ''}`
+      : (process.env.NEXT_PUBLIC_SERVER_URL ?? ''),
+  );
 
   const { blocks } = data;
 
