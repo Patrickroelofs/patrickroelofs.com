f897b9b3596588c3466bc28ece58a00984b31944
Patrick Roelofs <contact@patrickroelofs.com>
2024-12-03 21:45:22 +0100

    fix: add tailwind.config.js for fallback

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/* Note: this config is a fallback to add safeList support to tailwind 4.0 beta 1, it should be removed when an alternative is available */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  safeList: [
+    'my-fluid-3xs',
+    'my-fluid-2xs',
+    'my-fluid-xs',
+    'my-fluid-sm',
+    'my-fluid-md',
+    'my-fluid-lg',
+    'my-fluid-xl',
+    'my-fluid-2xl',
+    'my-fluid-3xl',
+    'my-fluid-4xl',
+    'my-fluid-5xl',
+    'my-fluid-6xl',
+    'my-fluid-7xl',
+    'my-fluid-8xl',
+    'my-fluid-9xl',
+  ],
+};
