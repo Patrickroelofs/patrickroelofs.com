1fac7bdc8af67030bd1f282baa9360b1db8d42c4
Patrick Roelofs <contact@patrickroelofs.com>
2025-02-13 23:02:29 +0100

    update revalidation

--- src/util/revalidateAfterChange.ts
+++ src/util/revalidateAfterChange.ts
@@ -2,12 +2,15 @@ import { revalidatePath } from "next/cache";
 
 function revalidateAfterChange({
   doc,
+  previousDoc,
 }: {
   doc: {
     slug: string;
+    _status: "published" | "draft";
   };
+  previousDoc: unknown;
 }) {
-  if (doc.slug) {
+  if (doc.slug && doc._status === "published" && doc !== previousDoc) {
     console.log("Revalidating...");
     revalidatePath("/", "layout");
   }
