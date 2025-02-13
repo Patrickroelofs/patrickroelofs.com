ab3546746a622090d22d2c7dbae299d9a8f69d9a
Patrick Roelofs <contact@patrickroelofs.com>
2025-02-13 16:27:55 +0100

    add migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20250212_204811 from './20250212_204811';
 import * as migration_20250212_210754 from './20250212_210754';
 import * as migration_20250212_214845 from './20250212_214845';
 import * as migration_20250212_215414 from './20250212_215414';
+import * as migration_20250213_152749 from './20250213_152749';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20250212_215414.up,
     down: migration_20250212_215414.down,
-    name: '20250212_215414'
+    name: '20250212_215414',
+  },
+  {
+    up: migration_20250213_152749.up,
+    down: migration_20250213_152749.down,
+    name: '20250213_152749'
   },
 ];
