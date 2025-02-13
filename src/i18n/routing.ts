c75281fd0c90857c9724c98df717b131a1780ccd
Patrick Roelofs <contact@patrickroelofs.com>
2025-02-13 21:58:12 +0100

    fix locale routing type error

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,11 +3,13 @@ import { defineRouting } from "next-intl/routing";
 import localization from "./localization";
 
 export const routing = defineRouting({
-  locales: localization.locales.map((locale) => locale.code),
+  locales: localization.locales.map((locale) => {
+    if (typeof locale === "string") throw new Error("Invalid locale");
+
+    return locale.code;
+  }),
   defaultLocale: localization.defaultLocale,
 });
 
 export const { Link, redirect, usePathname, useRouter } =
   createNavigation(routing);
-
-export type Locale = (typeof routing.locales)[number];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { Blog } from "@/collections/blog";
 import { Media } from "@/collections/media";
 import { Pages } from "@/collections/pages";
@@ -5,8 +7,6 @@ import { Users } from "@/collections/users";
 import { vercelPostgresAdapter } from "@payloadcms/db-vercel-postgres";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { buildConfig } from "payload";
 import sharp from "sharp";
 import { Navigation } from "./globals/navigation";
