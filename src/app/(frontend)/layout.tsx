93848834a654ecc7100bf91e62607b23c66f99f7
Patrick Roelofs <contact@patrickroelofs.com>
2025-02-13 16:24:29 +0100

    add navigation component

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,10 +2,11 @@ import { Analytics } from "@vercel/analytics/react";
 import type React from "react";
 import type { ReactElement } from "react";
 
+import { Navigation } from "@/patterns/navigation/navigation";
 import "@/styles/globals.css";
 import "@fontsource-variable/literata";
-import "@fontsource-variable/literata/opsz.css";
 import "@fontsource-variable/literata/opsz-italic.css";
+import "@fontsource-variable/literata/opsz.css";
 import "@fontsource-variable/literata/wght-italic.css";
 
 export const metadata = {};
@@ -18,6 +19,7 @@ export default function RootLayout(props: {
   return (
     <html lang="en" className="bg-ginger font-serif text-black">
       <body>
+        <Navigation />
         <main>{children}</main>
         <Analytics />
       </body>
