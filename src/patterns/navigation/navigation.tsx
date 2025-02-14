1314d2fbcf6ef04f282e0e7d0d670423fe94724d
Patrick Roelofs <contact@patrickroelofs.com>
2025-02-14 12:54:28 +0100

    style: update navigation with fade styling (#38)

--- src/patterns/navigation/navigation.tsx
+++ src/patterns/navigation/navigation.tsx
@@ -14,33 +14,31 @@ async function Navigation(props: NavigationType) {
   });
 
   return (
-    <nav className="sticky top-2 mt-12 z-50">
-      <div className="relative container mx-auto grid gap-4 md:grid-cols-2 border-2 bg-ginger border-black py-4 items-center rounded-t-lg">
-        <Link href="/" className="text-2xl font-bold">
-          <h1>Patrick Roelofs</h1>
-        </Link>
-        <div className="flex md:justify-end">
-          <ul className="flex space-x-6">
-            {data.navigation?.links?.map((link) => {
-              if (link.link?.relationTo === "pages") {
-                const pageLink = link.link.value as Page;
+    <nav>
+      <Link href="/" className="text-2xl font-bold">
+        <h1>Patrick Roelofs</h1>
+      </Link>
+      <div className="flex md:justify-end">
+        <ul className="flex space-x-6">
+          {data.navigation?.links?.map((link) => {
+            if (link.link?.relationTo === "pages") {
+              const pageLink = link.link.value as Page;
 
-                return (
-                  <li key={link.id}>
-                    <Link
-                      href={`/${pageLink.slug === "home" ? "" : pageLink.slug}`}
-                      className="text-xl font-medium"
-                    >
-                      {link.overrideLabel ? link.label : pageLink.title}
-                    </Link>
-                  </li>
-                );
-              }
+              return (
+                <li key={link.id}>
+                  <Link
+                    href={`/${pageLink.slug === "home" ? "" : pageLink.slug}`}
+                    className="text-xl font-medium"
+                  >
+                    {link.overrideLabel ? link.label : pageLink.title}
+                  </Link>
+                </li>
+              );
+            }
 
-              return null;
-            })}
-          </ul>
-        </div>
+            return null;
+          })}
+        </ul>
       </div>
     </nav>
   );
