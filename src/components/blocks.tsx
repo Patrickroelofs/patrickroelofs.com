b33da8426b13654e804ebae18b70cdc5689cea5e
Patrick Roelofs <contact@patrickroelofs.com>
2025-02-14 15:10:12 +0100

    Add generic blocks renderer Fixes #42

---
+++ src/components/blocks.tsx
@@ -0,0 +1,31 @@
+import type { ComponentType } from "react";
+
+interface BlocksProps {
+  blocks: [] | null | undefined;
+}
+
+const blockComponents = {};
+
+function Blocks({ blocks }: BlocksProps) {
+  if (blocks === null || blocks === undefined) {
+    return null;
+  }
+
+  return (
+    <>
+      {blocks.map((block) => {
+        const { blockType } = block;
+
+        if (blockType in blockComponents) {
+          const BlockComponent = blockComponents[blockType] as ComponentType;
+
+          return <BlockComponent key={block.id} {...block} />;
+        }
+
+        return null;
+      })}
+    </>
+  );
+}
+
+export { Blocks };
