a1cbe1f691a26becf1112f9c998f4eb3b6cf65f3
Patrick Roelofs <contact@patrickroelofs.com>
2025-02-20 22:44:33 +0100

    chore: update payloadcms to 3.24 (#74)

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,23 +1,14 @@
+import type { Icons } from "@/payload-types";
 import * as PhosphorIcons from "@phosphor-icons/react/dist/ssr";
-import type { ComponentProps, ComponentType, ReactElement } from "react";
-
-type IconName = keyof typeof PhosphorIcons;
+import type { ReactElement } from "react";
 
 interface IconType {
   size: number;
-  name: IconName;
-}
-
-function isValidIcon(name: string): name is IconName {
-  return name in PhosphorIcons;
+  name: Icons;
 }
 
 function Icon(props: IconType): ReactElement | null {
-  if (!isValidIcon(props.name)) return null;
-
-  const IconComponent = PhosphorIcons[props.name] as ComponentType<
-    Omit<ComponentProps<(typeof PhosphorIcons)[IconName]>, "weights">
-  >;
+  const IconComponent = PhosphorIcons[props.name];
 
   return <IconComponent weight="duotone" {...props} />;
 }
