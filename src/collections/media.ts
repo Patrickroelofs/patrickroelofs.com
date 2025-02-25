397be6518f778e74929f0bad003387e1c7d8a146
Patrick Roelofs <contact@patrickroelofs.com>
2025-02-25 21:25:25 +0100

    ci: reduce cost by moving to railway (#77)

--- src/collections/media.ts
+++ src/collections/media.ts
@@ -1,5 +1,10 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { generateBlurData } from "@/util/generateBlurData";
 import type { CollectionConfig } from "payload";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export const Media: CollectionConfig = {
   slug: "media",
@@ -23,7 +28,9 @@ export const Media: CollectionConfig = {
       },
     },
   ],
-  upload: true,
+  upload: {
+    staticDir: path.resolve(dirname, "../../public/media"),
+  },
   hooks: {
     beforeValidate: [generateBlurData],
   },
