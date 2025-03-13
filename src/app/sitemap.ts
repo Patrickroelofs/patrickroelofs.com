e14e1f3cba9d535266bcf8d01c1927ffe9edf876
Patrick Roelofs <contact@patrickroelofs.com>
2025-03-13 17:47:40 +0100

    feat: implement seo improvements on every page (#123)

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { payload } from "@/util/getPayloadConfig";
+import { getServersideURL } from "@/util/getServersideURL";
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const url = getServersideURL();
+
+  // TODO: Add blog posts to sitemap
+  const pages = await payload.find({
+    collection: "pages",
+    limit: 0,
+    where: {},
+  });
+
+  return [
+    ...pages.docs.map(({ slug, updatedAt }) => ({
+      url: `${url}/${slug === "home" ? "" : slug}`,
+      lastModified: new Date(updatedAt),
+    })),
+  ];
+}
