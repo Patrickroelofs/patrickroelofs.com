374ecdd468ed614933d6211d6a378d862054f2bb
Patrick Roelofs <contact@patrickroelofs.com>
2025-03-13 22:02:30 +0100

    feat: create structured data for blog articles (#125)

---
+++ src/components/Schema/index.tsx
@@ -0,0 +1,29 @@
+import type { Blog, Media, User } from "@/payload-types";
+import { getServersideURL } from "@/util/getServersideURL";
+
+function articleSchema(props: Blog) {
+  const image = props.meta?.image as Media;
+
+  return {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    headline: props.title,
+    datePublished: new Date(props.createdAt),
+    dateModified: new Date(props.updatedAt),
+    image: getServersideURL() + image?.url,
+    author: {
+      "@type": "Person",
+      name: "Patrick Roelofs",
+    },
+  };
+}
+
+function imageSchema(props: Media) {
+  return {
+    "@context": "https://schema.org",
+    "@type": "ImageObject",
+    contentUrl: getServersideURL() + props.url,
+  };
+}
+
+export { articleSchema, imageSchema };
