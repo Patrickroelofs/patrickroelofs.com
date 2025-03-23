397804194990ef6481934210f318f02f04032b82
Patrick Roelofs <contact@patrickroelofs.com>
2025-03-23 22:35:57 +0100

    add custom media support

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,20 @@
 export default {
-  plugins: [],
+  plugins: {
+    "@csstools/postcss-global-data": {
+      files: ["./src/styles/custommedia.css"],
+    },
+    "postcss-custom-media": {
+      preserve: true,
+    },
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    },
+  },
 };
