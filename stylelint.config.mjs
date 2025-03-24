676d2fa01d0b47cab2a1aa93aae396a703cb53f0
Patrick Roelofs <contact@patrickroelofs.com>
2025-03-24 22:39:03 +0100

    implement stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "at-rule-no-unknown": null,
+    "selector-class-pattern": null,
+  },
+};
