386eb24d2c69b3013fe3acaaf5a52a7f3c857cb8
Patrick Roelofs <contact@patrickroelofs.com>
2025-04-05 00:25:38 +0200

    Update playwright.config.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,8 +45,8 @@ export default defineConfig({
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
