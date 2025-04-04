77807c7cdba81d6a23693b30059092aaf8ef8e9d
Patrick Roelofs <contact@patrickroelofs.com>
2025-04-05 00:25:49 +0200

    Update playwright.config.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,8 +40,8 @@ export default defineConfig({
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
