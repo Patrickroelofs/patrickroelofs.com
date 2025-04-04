1ee99f7764a3f209d568966a4a1b75cd7b8cdbb5
Patrick Roelofs <contact@patrickroelofs.com>
2025-04-04 23:42:17 +0200

    fix stylelint

--- stylelint.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: ["stylelint-config-standard"],
-  rules: {
-    "at-rule-no-unknown": null,
-    "selector-class-pattern": null,
-  },
-};
