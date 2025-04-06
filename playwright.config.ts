929a652e62d623ca9f47d3d1d3e8263fd859b514
Patrick Roelofs <contact@patrickroelofs.com>
2025-04-06 23:05:38 +0200

    Update playwright.config.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,8 +35,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
