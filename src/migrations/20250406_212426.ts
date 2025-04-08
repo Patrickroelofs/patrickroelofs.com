37637a7dd330f5f6bd54ef3c8e56c5e038e20f3a
Patrick Roelofs <contact@patrickroelofs.com>
2025-04-08 17:23:28 +0200

    ERROR: Error running migration 20250406_212426 column "label" of relation "footer_social_links" contains null values. Fixes #176

--- src/migrations/20250406_212426.ts
+++ src/migrations/20250406_212426.ts
@@ -2,7 +2,7 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "footer_social_links" ADD COLUMN "label" varchar NOT NULL;`)
+   ALTER TABLE "footer_social_links" ADD COLUMN "label" varchar NOT NULL DEFAULT 'replace with value';`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
