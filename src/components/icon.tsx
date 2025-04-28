c14e28d16e2a84f341561b0579652494e555da3d
Patrick Roelofs <contact@patrickroelofs.com>
2025-04-28 19:03:43 +0200

    feat: add SiteLink and IconLink components with styling (#44)
    * feat: add SiteLink and IconLink components with styling
    
    * feat: update CSS configurations and formatting for consistency
    
    * fix: reorder import statements for consistency

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -5,12 +5,15 @@ import type { ReactElement } from "react";
 interface IconType {
 	size: number;
 	name: Icons;
+	className?: string;
 }
 
 function Icon(props: IconType): ReactElement | null {
 	const IconComponent = PhosphorIcons[props.name];
 
-	return <IconComponent {...props} weight="duotone" />;
+	return (
+		<IconComponent {...props} weight="duotone" className={props.className} />
+	);
 }
 
 export { Icon };
---
+++ src/components/link/link.tsx
@@ -0,0 +1,48 @@
+import type { Icons } from "@/payload-types";
+import Link from "next/link";
+import { Icon } from "../icon";
+import styles from "./link.module.css";
+
+export type SiteLinkProps = {
+	children: React.ReactNode;
+	href: string;
+	className?: string;
+	target?: string;
+};
+
+const SiteLink = (props: SiteLinkProps) => {
+	return (
+		<Link href={props.href} target={props.target} className={styles.link}>
+			{props.children}
+			{props.target === "_blank" && (
+				<span className="visually-hidden">opens in a new tab</span>
+			)}
+			<Icon
+				aria-hidden={true}
+				className={styles.icon}
+				size={24}
+				name={props.target === "_blank" ? "ArrowSquareOut" : "LinkSimple"}
+			/>
+		</Link>
+	);
+};
+
+type IconLinkProps = {
+	size: number;
+	icon: Icons;
+};
+
+const IconLink = (props: SiteLinkProps & IconLinkProps) => {
+	return (
+		<Link href={props.href} target={props.target}>
+			<span className="visually-hidden">
+				{props.target === "_blank"
+					? `${props.children} opens in new tab`
+					: props.children}
+			</span>
+			<Icon aria-hidden={true} size={props.size} name={props.icon} />
+		</Link>
+	);
+};
+
+export { SiteLink, IconLink };
