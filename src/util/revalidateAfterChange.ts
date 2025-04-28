18932f4f6926ea5e47baea5a886f97f42292ae14
Patrick Roelofs <contact@patrickroelofs.com>
2025-04-28 19:36:48 +0200

    style: format code for consistency in revalidateAfterChange function

--- src/util/revalidateAfterChange.ts
+++ src/util/revalidateAfterChange.ts
@@ -1,19 +1,19 @@
 import { revalidatePath } from "next/cache";
 
 function revalidateAfterChange({
-  doc,
-  previousDoc,
+	doc,
+	previousDoc,
 }: {
-  doc: {
-    slug: string;
-    _status: "published" | "draft";
-  };
-  previousDoc: unknown;
+	doc: {
+		slug: string;
+		_status: "published" | "draft";
+	};
+	previousDoc: unknown;
 }) {
-  if (doc.slug && doc._status === "published" && doc !== previousDoc) {
-    console.log("Revalidating...");
-    revalidatePath("/", "layout");
-  }
+	if (doc.slug && doc._status === "published" && doc !== previousDoc) {
+		console.log("Revalidating...");
+		revalidatePath("/", "layout");
+	}
 }
 
 export { revalidateAfterChange };
