bee1395c83bd3a7845d57a7e890f9326da0d7048
Patrick Roelofs <contact@patrickroelofs.com>
2025-05-03 22:59:18 +0200

    feat: add AnimatedTitle component and integrate into StickyTitle (#68)
    * feat: add AnimatedTitle component and integrate into StickyTitle
    
    * fix: streamline onlyBuiltDependencies format in package.json

--- src/components/sticky-title/sticky-title.tsx
+++ src/components/sticky-title/sticky-title.tsx
@@ -2,15 +2,14 @@ import { Blocks } from "@/components/blocks";
 import type { StickyTitleType } from "@/payload-types";
 import { cva } from "class-variance-authority";
 import styles from "./sticky-title.module.css";
+import { AnimatedTitle } from "./elements/animated-title";
 
 const stickyTitleStyles = cva(styles.container);
 
 function StickyTitle(props: StickyTitleType) {
 	return (
 		<section className={stickyTitleStyles()}>
-			<div>
-				<h2 className={styles.title}>{props.title}</h2>
-			</div>
+			<AnimatedTitle>{props.title}</AnimatedTitle>
 			<div>
 				<Blocks blocks={props.blocks} />
 			</div>
