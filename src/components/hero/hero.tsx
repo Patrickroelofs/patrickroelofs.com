6f71850c9ffb1b0e63cb1e6d70f033cf2c04c66a
Patrick Roelofs <contact@patrickroelofs.com>
2025-05-07 22:10:09 +0200

    feat: add animation to hero section (#70)
    * add animation to hero
    Fixes #69
    
    * fix: ensure animation triggers only if imageWrapperRef is available
    
    * update payload

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,17 +1,54 @@
+"use client";
+
 import type { HeroBlockType, Media } from "@/payload-types";
 import { Image } from "@/components/image";
 import styles from "./hero.module.css";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-async function Hero(props: HeroBlockType) {
+gsap.registerPlugin(ScrollTrigger);
+
+function Hero(props: HeroBlockType) {
 	const { image } = props as {
 		image: Media;
 	};
 
+	const imageWrapperRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				markers: process.env.NODE_ENV === "development",
+				trigger: "body",
+				start: "94px top",
+				end: "+=480px top",
+				scrub: true,
+			},
+		});
+
+		if (imageWrapperRef.current) {
+			tl.to(imageWrapperRef.current, {
+				scale: 1,
+				y: "-5%",
+				ease: "sine.inOut",
+				duration: 0.75,
+			});
+		}
+
+		return () => {
+			tl.kill();
+		};
+	}, []);
+
 	return (
 		<header className={styles.hero}>
-			<p className={styles.title}>{props.title}</p>
-			<p className={styles.subtitle}>{props.subtitle}</p>
-			<div className={styles.wrapper}>
+			<div className={styles.container}>
+				<p className={styles.title}>{props.title}</p>
+				<p className={styles.subtitle}>{props.subtitle}</p>
+			</div>
+
+			<div className={styles.wrapper} ref={imageWrapperRef}>
 				<Image media={image} className={styles.image} fill />
 			</div>
 		</header>
--- src/components/sticky-title/sticky-title.tsx
+++ src/components/sticky-title/sticky-title.tsx
@@ -2,14 +2,13 @@ import { Blocks } from "@/components/blocks";
 import type { StickyTitleType } from "@/payload-types";
 import { cva } from "class-variance-authority";
 import styles from "./sticky-title.module.css";
-import { AnimatedTitle } from "./elements/animated-title";
 
 const stickyTitleStyles = cva(styles.container);
 
 function StickyTitle(props: StickyTitleType) {
 	return (
 		<section className={stickyTitleStyles()}>
-			<AnimatedTitle>{props.title}</AnimatedTitle>
+			<h2 className={styles.title}>{props.title}</h2>
 			<div>
 				<Blocks blocks={props.blocks} />
 			</div>
