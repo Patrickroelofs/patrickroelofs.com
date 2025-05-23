2264f28eefbdbe51f441e97c2337635fa60c5a45
Patrick Roelofs <contact@patrickroelofs.com>
2025-05-23 13:23:58 +0200

    Implement payload folders in media folder Fixes #108

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -4,6 +4,9 @@ import type { CollectionConfig } from "payload";
 export const Media: CollectionConfig = {
 	slug: "media",
 	upload: true,
+	admin: {
+		folders: true,
+	},
 	access: {
 		read: () => true,
 	},
