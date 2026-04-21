9b651150dc5837237c8fe53a59a04f27885bd5e7
Patrick Roelofs <patrick.roelofs@iquality.nl>
2026-04-21 16:28:15 +0200

    setup fonts and icons

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -35,7 +35,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 			<head>
 				<HeadContent />
 			</head>
-			<body>
+			<body className="bg-ginger font-sans text-dark-grey">
 				{children}
 				<TanStackDevtools
 					config={{
